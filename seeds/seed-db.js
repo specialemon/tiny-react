653c61567ece9e6e9797b911936a0a88b34b61f4
Haoyuan Lin <specialemon@gmail.com>
2019-03-22 12:39:36 -0400

    deploy

--- seeds/seed-db.js
+++ seeds/seed-db.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://specialemon:199199@cluster0-shard-00-00-qskyf.mongodb.net:27017,cluster0-shard-00-01-qskyf.mongodb.net:27017,cluster0-shard-00-02-qskyf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true, useMongoClient: true }).then(function () {
+mongoose.connect("mongodb://specialemon:199199@cluster0-shard-00-00-qskyf.mongodb.net:27017,cluster0-shard-00-01-qskyf.mongodb.net:27017,cluster0-shard-00-02-qskyf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true}).then(function () {
     console.log("connected to db");
 });
 
 const User = require('../models/User');
 const Kudo = require('../models/Kudo');
 
-User.deleteMany();
-Kudo.deleteMany();
+User.deleteMany({});
+Kudo.deleteMany({});
 
 User.create([{
     Name: "Leon"
