5ef20185bc87cd926f3d35544bf44855c02f1d90
Haoyuan Lin <specialemon@gmail.com>
2019-03-22 12:21:41 -0400

    deploy fix

--- seeds/seed-db.js
+++ seeds/seed-db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/kudoTest", { useNewUrlParser: true, useMongoClient: true }).then(function () {
+mongoose.connect("mongodb://specialemon:199199@cluster0-shard-00-00-qskyf.mongodb.net:27017,cluster0-shard-00-01-qskyf.mongodb.net:27017,cluster0-shard-00-02-qskyf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true, useMongoClient: true }).then(function () {
     console.log("connected to db");
 });
 
--- server.js
+++ server.js
@@ -7,15 +7,23 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./client/public"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 
-
-mongoose.connect("mongodb://localhost/kudoTest", { useNewUrlParser: true}).then(function(){
+mongoose.connect("mongodb://specialemon:199199@cluster0-shard-00-00-qskyf.mongodb.net:27017,cluster0-shard-00-01-qskyf.mongodb.net:27017,cluster0-shard-00-02-qskyf.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true", { useNewUrlParser: true}).then(function(){
   console.log("connected to db");
 });
 
 require('./routes/apiRoutes')(app);
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", function (req,res){
+      res.sendFile(__dirname + "/client/build/index.html")
+  })
+}
 
 
 app.listen(PORT, function() {
